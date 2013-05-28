be7bb003d30fbe5cf45c3ae8fb1de06d409b7a37
Aaron Trostle <aaron.trostle@gmail.com>
2013-05-28 14:16:17 -0700

    Add user resource via Devise.

---
+++ spec/integration/signing_up_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+feature 'Signing up' do
+  scenario 'Successful sign up' do
+    visit '/'
+    click_link 'Sign up'
+    fill_in 'Email', :with => 'instructor@codefellows.org'
+    fill_in 'Password', :with => 'password'
+    fill_in 'Password confirmation', :with => 'password'
+    click_button 'Sign up'
+    message = 'Please open the link to activate your account.'
+    page.should have_content('Please confirm your account ' +
+                             'before signing in.')
+  end
+
+end
